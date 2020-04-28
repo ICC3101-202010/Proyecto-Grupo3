83e7d4a2cf58fac9a20bdfa01ea4f0d8af0b9644
maserrasm <maserra@miuandes.cl>
2020-04-28 11:09:34 -0400

    Planteo Inicial Clase Usuario

---
+++ Entrega2/Usuario.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entrega2
+{
+    abstract class Usuario
+    {
+        //Parametros base de un usuario.
+
+        int idUsuario; int metodoPago; string nombre; string apellido;
+        string claveUsuario; string mail; string descripcion; bool privado;
+        bool premium;
+
+        public Usuario(int idUsuario, int metodoPago, string nombre, string apellido, string claveUsuario, string mail, string descripcion, bool privado, bool premium)
+        {
+            this.idUsuario = idUsuario; this.metodoPago = metodoPago; this.nombre = nombre; this.apellido = apellido;
+            this.claveUsuario = claveUsuario; this.mail = mail; this.descripcion = descripcion; this.privado = privado;
+            this.premium = premium;
+        }
+
+        //Playlists de Usuario.
+
+        //List<Playlist> Playlists;
+        //List<PlaylistCancion> CancionesPublicas;
+        //List<PlaylistCancion> CancionesPrivadas;
+        //List<PlaylistVideo> VideosPublicos;
+        //List<PlaylistVideo> VideosPrivados;
+
+        //Cola de canciones y videos.
+
+        //List<Cancion> ColaCanciones;
+        //List<Video> ColaVideos;
+
+    }
+
+
+    class Actor : Usuario
+    {
+        //List<Videos> Peliculas;
+        List<String> Premios;
+
+    }
+}
